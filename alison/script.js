17f769a75dd8d0341411b2b46e69a5b8b470d3d6
kruemel <alison.thomas.sound@gmail.com>
2018-02-25 14:51:11 +0100

    made bomb countdown

--- alison/script.js
+++ alison/script.js
@@ -10,12 +10,35 @@ function startTime() {
     var m = today.getMinutes();
     var s = today.getSeconds();
     
+    m = checkTime(m);
+    s = checkTime(s);
+
     $("#clock").html(h + ":" + m + ":" + s);
 
-    
+    setTimeout(startTime, 500);
 }
 
-function changeColor() {
-    console.log("I clicked!");
-    $("body").css("background-color", "pink");
+function checkTime(number){
+    if(number < 10){
+        number = "0"+number;
+    }
+    return number;
 }
+
+function startBomb() {
+    var bombSeconds = 11;
+    tick(bombSeconds);
+}
+
+function tick(bombSeconds) {
+   bombSeconds = bombSeconds - 1;
+   setTimeout(function(){
+        if (bombSeconds === 0){
+            $("#bomb").html("BOOM!");
+        } else {
+            tick(bombSeconds);
+            $("#bomb").html(bombSeconds);
+        }
+    }, 1000)
+}
+
